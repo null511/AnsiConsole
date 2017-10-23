1c5c3df7488870543a989da6ea25bb519ffe1733
JoshuaMiller <admin@joshuamiller.info>
2017-10-22 20:07:30 -0400

    initial

---
+++ AnsiConsole/AnsiConsole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnsiConsole
+{
+    public static class AnsiConsole
+    {
+        public static AnsiWriter Out {get;}
+        public static AnsiWriter Error {get;}
+
+
+        static AnsiConsole()
+        {
+            Out = new AnsiWriter(Console.Out);
+            Error = new AnsiWriter(Console.Error);
+        }
+    }
+}
---
+++ AnsiConsole/AnsiWriter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace AnsiConsole
+{
+    public class AnsiWriter
+    {
+        public TextWriter Writer {get;}
+
+
+        public AnsiWriter(TextWriter writer)
+        {
+            this.Writer = writer;
+        }
+
+        public AnsiWriter Write(string text)
+        {
+            Writer.Write(text);
+            return this;
+        }
+
+        public AnsiWriter WriteLine()
+        {
+            Writer.WriteLine();
+            return this;
+        }
+
+        public AnsiWriter Write(string text, ConsoleColor color)
+        {
+            SetColor(color);
+            Writer.Write(text);
+            return this;
+        }
+
+        public AnsiWriter WriteLine(string text, ConsoleColor color)
+        {
+            SetColor(color);
+            Writer.WriteLine(text);
+            return this;
+        }
+
+        public AnsiWriter SetColor(ConsoleColor color)
+        {
+            //
+            return this;
+        }
+    }
+}
